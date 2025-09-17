64f1b4d6bd386f1cf9e4f9498435e4bb5a9b30a4
Andrew Josephs <ajosephs1@users.noreply.github.com>
2025-09-16 22:02:01 -0700

    Integrate blog & update projects

---
+++ src/consts.ts
@@ -0,0 +1,44 @@
+import type { Site, Metadata, Socials } from "@types";
+
+export const SITE: Site = {
+  NAME: "DEVING OUT LOUD",
+  EMAIL: "andrew_josephs@hotmail.co.uk",
+  NUM_POSTS_ON_HOMEPAGE: 5,
+  // NUM_WORKS_ON_HOMEPAGE: 0,
+  // NUM_PROJECTS_ON_HOMEPAGE: 0,
+};
+
+export const HOME: Metadata = {
+  TITLE: "Home",
+  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+};
+
+export const BLOG: Metadata = {
+  TITLE: "Blog",
+  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+};
+
+export const WORK: Metadata = {
+  TITLE: "Work",
+  DESCRIPTION: "Where I have worked and what I have done.",
+};
+
+export const PROJECTS: Metadata = {
+  TITLE: "Projects",
+  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+};
+
+export const SOCIALS: Socials = [
+  { 
+    NAME: "twitter-x",
+    HREF: "https://twitter.com/markhorn_dev",
+  },
+  { 
+    NAME: "github",
+    HREF: "https://github.com/markhorn-dev"
+  },
+  { 
+    NAME: "linkedin",
+    HREF: "https://www.linkedin.com/in/markhorn-dev",
+  }
+];
